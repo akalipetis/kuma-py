0bea3db9595037b2db45a7c6a5e720c6aa61ad38
Paris Kasidiaris <pariskasidiaris@gmail.com>
2014-02-14 18:33:05 +0000

    Basic functionality is okay

--- kuma/document.py
+++ kuma/document.py
@@ -4,12 +4,17 @@ import requests
 
 class Document(object):
 
+    _dict_cache = None
+
     def __init__(self, client, endpoint):
         self.client = client
         self.endpoint = endpoint
 
-    def __getattr__(self, name):
+    def get_child(self, name):
         return Document(self.client, os.path.join(self.endpoint, name))
+
+    def __getattr__(self, name):
+        return self.get_child(name)
 
     def __str__(self):
         return 'Kuma Document: %s' % self.url
@@ -23,16 +28,8 @@ class Document(object):
         pass
 
     @property
-    def macros(self):
-        pass
-
-    @property
-    def nomacros(self):
-        pass
-
-    @property
     def summary(self):
-        pass
+        return self.json()['summary']
 
     def section(self, section_id):
         pass
@@ -43,15 +40,17 @@ class Document(object):
 
     def toc(self):
         response = self._get_meta('toc')
-        return response.test
+        return response.text
 
-    def json(self):
-        response = self._get_meta('json')
-        return response.json()
+    def json(self, no_cache=False):
+        if (not self._dict_cache or no_cache):
+            response = self._get_meta('json')
+            self._dict_cache = response.json()
+        return self._dict_cache
 
     def children(self):
         response = self._get_meta('children')
         return response.json()
 
     def html(self):
-        pass
+        return requests.get(self.url).text
