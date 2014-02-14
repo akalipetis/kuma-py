b0c558da9096f6824775bd07ee0c33bc3f9d7112
Paris Kasidiaris <pariskasidiaris@gmail.com>
2014-02-14 16:15:49 +0000

    Initial commit. Just playin around.

---
+++ kuma/client.py
@@ -0,0 +1,12 @@
+import os
+import requests
+
+
+class Client(object):
+    def __init__(self, host='https://developer.mozilla.org', locale='en-US'):
+        self.host = host
+        self.locale = locale
+
+    @property
+    def base_url(self):
+        return os.path.join(self.host, self.locale)
